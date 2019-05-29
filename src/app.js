b6fc05f51559f1fa50bc3500a95403b6632f7f24
Mostafa <mostafa.ghadimi@yahoo.com>
2019-05-30 02:50:37 +0430

    favicon added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class app extends Component {
+export default class App extends Component {
     render() {
         return (
             <div>
