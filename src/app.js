8d508884e8a0a6e9af7325553ed5a59205a1b4ab
Mostafa <mostafa.ghadimi@yahoo.com>
2019-05-30 03:17:51 +0430

    normalizer and fonts just created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import './assets/css/normalizer.css'
+import './assets/css/app.css'
+
 export default class App extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="app-container">
+                سلام
             </div>
         )
     }
