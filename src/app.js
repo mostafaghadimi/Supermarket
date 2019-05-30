8394964b6729fe73f3e22c4cdeab298055a7ebe1
Mostafa <mostafa.ghadimi@yahoo.com>
2019-05-30 05:13:54 +0430

    login page created

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 import './assets/css/normalizer.css'
 import './assets/css/app.css'
 
+import Login from './components/login/login'
+
 export default class App extends Component {
     render() {
         return (
-            <div className="app-container">
-                سلام
-            </div>
+            <Login />
         )
     }
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+import './../../assets/css/login.css'
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="login-container">
+                <div className="login-form">
+                    <img className="login-form-image" src={require('../../assets/img/login.jpg')} />
+                </div>
+                <div className="login-info">
+                    <div className="login-input">
+                        <input type="text" placeholder="نام کاربری"/>
+                    </div>
+                    <div className="login-input">
+                        <input type="password" placeholder="رمز عبور" />
+                    </div>
+                </div>
+                <div className="login-button-container">
+                    <button className="login-button">ورود</button>
+                </div>
             </div>
         )
     }
