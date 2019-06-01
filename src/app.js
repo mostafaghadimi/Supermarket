bb7389d94185db8570f808d0ba6823be805d1a8d
Mostafa <mostafa.ghadimi@yahoo.com>
2019-06-01 10:54:44 +0430

    react-router-dom added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import './assets/css/normalizer.css'
 import './assets/css/app.css'
@@ -9,7 +10,12 @@ import Addition from './components/supermarket/add/add-supermarket'
 export default class App extends Component {
     render() {
         return (
-            <Addition/>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/supermarket/add' component={Addition} />
+                    <Route exact path='/' component={Login} />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
