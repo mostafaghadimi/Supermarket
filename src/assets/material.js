b950ca6259eb775b6f3c01797cb7c0b1a1aefddb
Reza Zeiny <rezazeiny1998@gmail.com>
2019-06-18 01:41:10 +0430

    Change UI Base

---
+++ src/assets/material.js
@@ -0,0 +1,103 @@
+// import {APP_PRIMARY_COLOR} from "./AppDetail";
+import HomePageBackground from "./img/home_page_background.jpg"
+// import MapPageBackground from "../images/map1.jpg"
+// import GamePageBackground from "../images/gamePageBackground.jpg"
+
+export const flatButtonLabelStyle = {
+    color: 'white',
+    fontSize: '20px'
+};
+
+export const openGameFlatButtonLabelStyle = {
+    color: 'white',
+    fontSize: '20px',
+};
+export const flatButtonDivStyle = {
+    padding: '5px'
+};
+export const buttonThemeColorStyle = {
+    // backgroundColor: APP_PRIMARY_COLOR,
+};
+export const mainDiv = {
+    height: window.innerHeight,
+    width: window.innerWidth,
+};
+
+export const homePage = {
+    backgroundImage: "url(" + HomePageBackground + ")",
+    backgroundSize: "cover",
+    height: window.innerHeight,
+    width: window.innerWidth,
+    margin: "0",
+    display: "grid",
+    gridTemplateColumns: "37% 30% 33%",
+    gridTemplateRows: "70% 20% 10%"
+};
+
+export function divHomePageBlurBackground(blur) {
+    return {
+        backgroundImage: "url(" + HomePageBackground + ")",
+        backgroundSize: "cover",
+        position: "absolute",
+        filter: "blur(" + blur + "px)",
+        height: window.innerHeight,
+        width: window.innerWidth,
+        margin: "0",
+        zIndex: -1,
+    }
+}
+
+export const mainPageButton = {
+    gridColumn: "2 / 3",
+    gridRow: "2 / 3",
+};
+
+// export const divGamePageBlurBackground = {
+//         backgroundImage: "url(" + GamePageBackground + ")",
+//         backgroundSize: "cover",
+//         position: "absolute",
+//         filter: "blur(100px)",
+//         height: window.innerHeight,
+//         width: window.innerWidth,
+//         margin: "0",
+//         zIndex: -1,
+// };
+
+export const divGamePage = {
+// backgroundImage: "url(" + GamePageBackground + ")",
+    backgroundSize: "cover",
+    height: window.innerHeight,
+    width: window.innerWidth,
+    margin: "0",
+// display: "grid",
+// gridTemplateColumns: "37% 30% 33%",
+// gridTemplateRows: "70% 20% 10%"
+};
+
+export const gameMapDiv = {
+    // backgroundImage: "url(" + MapPageBackground + ")",
+    height: window.innerHeight * 0.75,
+    width: window.innerWidth * 0.6,
+    left: window.innerWidth * 0.05,
+    backgroundSize: "cover",
+    position: "absolute",
+    margin: "0",
+    // display: "inline-block",
+    boxShadow: "5px 5px 20px #79CBB2",
+};
+
+export const gameDetailDiv = {
+    height: window.innerHeight * 0.75,
+    width: window.innerWidth * 0.25,
+    left: window.innerWidth * 0.7,
+    backgroundSize: "cover",
+    position: "absolute",
+    margin: "0",
+    // display: "inline-block",
+};
+
+export const diceDiv = {
+    position: "relative",
+    top: window.innerHeight * 0.75,
+    textAlign: "center",
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import App from './app';
-import * as serviceWorker from './serviceWorker';
+import HomePage from './pages/home';
+import DashboardPage from './pages/dashboard';
+import CallbackPage from "./pages/callback";
+import Auth from "./components/Auth";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+ReactDOM.render(
+    <Auth>
+        <MuiThemeProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route path="/dashboard" component={DashboardPage}/>
+                    <Route path="/callback" component={CallbackPage}/>
+                </Switch>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    </Auth>,
+    document.getElementById('root')
+)
