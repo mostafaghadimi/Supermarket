55b709c4d59aa4aac0610604a80e64edbac33648
Mostafa <mostafa.ghadimi@yahoo.com>
2019-07-10 17:58:22 +0430

    index added

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Button, Input, Menu, Modal, Icon } from 'semantic-ui-react'
+import { Button, Input, Menu, Modal, Icon, Radio } from 'semantic-ui-react'
+import {Grid, Card, Image, Rating} from 'semantic-ui-react'
 
 import '../../assets/css/navigation.css'
+import '../../assets/css/index.css'
 export default class MenuExampleSizeSmall extends Component {
   state = { activeItem: 'home' }
 
@@ -11,70 +13,141 @@ export default class MenuExampleSizeSmall extends Component {
     const { activeItem } = this.state
 
     return (
-      <Menu size='small' className="navigation">
-        <Menu.Item name='خانه' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item
-          name='لیست فروشگاه‌ها'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
+      <div>
+        <Menu size='small' className="navigation">
+          <Menu.Item name='خانه' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name='لیست فروشگاه‌ها'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
 
-        <Menu.Menu position='left'>
-          <Menu.Item>
-            <Modal trigger={<Button color="green">ثبت‌‌نام</Button>} className="modal">
-                <Modal.Header className="modal-header">ثبت‌نام</Modal.Header>
-                <Modal.Content>
-                <Modal.Description>
-                    <Input iconPosition='right' placeholder='نام کاربری'>
-                        <Icon name='user' />
-                        <input />
-                    </Input>
-                    <br/>
-                    <br/>
-                    <Input iconPosition='right' placeholder='رمز عبور' type="password">
-                        <Icon name='key' />
-                        <input />
-                    </Input>
-                    <br/>
-                    <br/>
-                    <Button color="green">
-                        ثبت‌نام
-                    </Button>
-                </Modal.Description>
-                </Modal.Content>
-            </Modal>
-          </Menu.Item>
+          <Menu.Menu position='left'>
+            <Menu.Item>
+              <Modal trigger={<Button color="green">ثبت‌‌نام</Button>} className="modal">
+                  <Modal.Header className="modal-header">ثبت‌نام</Modal.Header>
+                  <Modal.Content>
+                  <Modal.Description>
+                      <Input iconPosition='right' placeholder='ایمیل' type="email">
+                          <Icon name='at' />
+                          <input />
+                      </Input>
+                      <br/>
+                      <br/>
+                      <Input iconPosition='right' placeholder='نام کاربری'>
+                          <Icon name='user' />
+                          <input />
+                      </Input>
+                      <br/>
+                      <br/>
+                      <Input iconPosition='right' placeholder='رمز عبور' type="password">
+                          <Icon name='key' />
+                          <input />
+                      </Input>
+                      <br/>
+                      <br/>
+                      <Button color="green">
+                          ثبت‌نام
+                      </Button>
+                  </Modal.Description>
+                  </Modal.Content>
+              </Modal>
+            </Menu.Item>
 
+            <Menu.Item>
+              <Modal trigger={<Button primary>ورود</Button>}>
+                  <Modal.Header className='modal-header'>ورود</Modal.Header>
+                  <Modal.Content>
+                  <Modal.Description>
+                      <Input iconPosition='right' placeholder='نام کاربری'>
+                          <Icon name='user' />
+                          <input />
+                      </Input>
+                      <br/>
+                      <br/>
+                      <Input iconPosition='right' placeholder='رمز عبور' type="password">
+                          <Icon name='key' />
+                          <input />
+                      </Input>
+                      <br/>
+                      <br/>
+                      <div className="remember">
+                        <Radio toggle />  
+                        <span>
+                          مرا به خاطر بسپار
+                        </span>
+                      </div>
+
+                      <br/>
+                      <br/>
+                      <Button primary>
+                          ورود
+                      </Button>
+                  </Modal.Description>
+                  </Modal.Content>
+              </Modal>
+            </Menu.Item>
+            
+          </Menu.Menu>
           <Menu.Item>
-            <Modal trigger={<Button color="primary">ورود</Button>}>
-                <Modal.Header className='modal-header'>ورود</Modal.Header>
-                <Modal.Content>
-                <Modal.Description>
-                    <Input iconPosition='right' placeholder='نام کاربری'>
-                        <Icon name='user' />
-                        <input />
-                    </Input>
-                    <br/>
-                    <br/>
-                    <Input iconPosition='right' placeholder='رمز عبور'>
-                        <Icon name='key' />
-                        <input />
-                    </Input>
-                    <br/>
-                    <br/>
-                    <Button color="primary">
-                        ورود
-                    </Button>
-                </Modal.Description>
-                </Modal.Content>
-            </Modal>
+            <Input icon='search' placeholder='جست‌و‌جو'/>
           </Menu.Item>
+        </Menu>
+        <Index/>
+      </div>
+    )
+  }
+}
+
+
+
+// export default class Index extends Component {
+class Index extends Component {
+  render() {
+    return (
+      <div className="index">
+        <div className="index-container">
+          <IndexItem/>
+          <IndexItem/>
+          <IndexItem/>
+          <IndexItem/>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
+
+class IndexItem extends Component {
+  render() {
+    return (
+      <div className="index-item">
+            <div className="index-image">
+              <img src={require('../../assets/img/ok.jpg')}/>
+            </div>
+
+            <div className="index-title">
+              <div>
+                <span className="index-icon">
+                  <Icon name="shopping basket"/>
+                </span>
+                فروشگاه سعید
+              </div>
+              <div className="index-rating">
+                <Rating defaultRating={4} maxRating={5} disabled />
+              </div>
+            </div>
+            <div className="index-address">
+              <span className="index-icon">
+                <Icon name="map marker alternate"/>
+              </span>
+                خ پیروزی، خ پرستار، پارک پرستار
+            </div>
+
+          </div>
           
-        </Menu.Menu>
-        <Menu.Item>
-          <Input icon='search' placeholder='جست‌و‌جو'/>
-        </Menu.Item>
-      </Menu>
     )
   }
 }
