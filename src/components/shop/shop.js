45e91ec8a95cfef75811ce547b6488d7fc7e6828
mohamadreza jafarzade <mrezajafarzade98@gmail.com>
2019-08-05 10:56:51 +0430

    fourth sprint

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,86 +1,161 @@
-import React, { Component } from 'react'
-import {Icon} from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Icon, Rating} from 'semantic-ui-react'
 
 import Comments from '../comment/comment'
 import AddComment from '../comment/addcomment'
 import '../../assets/css/shopinfo.css'
 
 export default class ShopPage extends Component {
+    static defaultProps = {};
+
+    state = {
+        market: {
+            comments_count: 3,
+            rates_result: 3,
+            comments: []
+        },
+    };
+
+    handleRate = (e, {rating}) => {
+        const item = JSON.parse(localStorage.getItem("USER_INFO"));
+        const marketId = JSON.parse(localStorage.getItem("market_id"));
+
+        fetch('http://192.168.194.100:8000/market/rate/', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: marketId,
+                owner: item.id,
+                rate: rating,
+                api: item.api,
+            }),
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            credentials: 'same-origin'
+        })
+            .then(res => {
+                if (res.status === 503) {
+                    alert("شما قبلا به این فروشگاه نظر داده اید!")
+                }
+                return res.json()
+            }).then((data) => {
+            console.log(data);
+        })
+            .catch(console.log);
+    };
+
+    componentDidMount() {
+        const marketId = JSON.parse(localStorage.getItem("market_id"));
+        fetch('http://192.168.194.100:8000/market/detail/', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: marketId,
+            }),
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            credentials: 'same-origin'
+        })
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({market: data});
+                console.log(data);
+            })
+            .catch(console.log);
+    }
+
     render() {
-      return (
-        <div className="shop-page-container">
-          <div className="shop-info">
-            <div className="shop-info-contact">
-              <div className="shop-name">
-                فروشگاه سعید
-              </div>
-              
-              <ShopInfo info="021-77401767" icon="phone" color="green"/>
-              <ShopInfo info="خ پیروزی، خ پرستار، پارک پرستار" icon="map marker alternate" color="red"/>
-              <ShopInfo info="0 نظر" icon="comment" color="yellow"/>
-            </div>
-            <div className="shop-image">
-              <img src={require('../../assets/img/ok.jpg')}/>
-            </div>
-          </div>
-          <div className="shop-products">
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-            <ShopProduct image={require('../../assets/img/taj.jpg')}  title="شوینده‌ی لباس تاژ" price="20 هزار تومان"/>
-          </div>
-          <div>
-            <Comments author="مصطفی قدیمی"  date="1 روز پیش" text="خیلی سوپری خوبیه."/>
-            <AddComment/>
-          </div>
-        </div>
-      )
+        return (
+            <React.Fragment>
+                <div className="shop-page-container">
+                    <div className="shop-info">
+                        <div className="shop-info-contact">
+                            <div className="shop-name">
+                                {this.state.market.market_name}
+                            </div>
+
+                            <ShopInfo info={this.state.market.phone_number} icon="phone" color="green"/>
+                            <ShopInfo info={this.state.market.address} icon="map marker alternate" color="red"/>
+                            <ShopInfo info={this.state.market.comments_count + " نظر"} icon="comment" color="yellow"/>
+                        </div>
+                        <div className="shop-image">
+                            <img src={"http://192.168.194.100:8000" + this.state.market.image}/>
+                        </div>
+                    </div>
+                    <div className="shop-products">
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                        <ShopProduct image={require('../../assets/img/taj.jpg')} title="شوینده‌ی لباس تاژ"
+                                     price="20 هزار تومان"/>
+                    </div>
+                    <div>
+                        <Rating defaultRating={this.state.market.rates_result} maxRating={5} onRate={this.handleRate}/>
+                        {
+                            this.state.market.comments.map(({id, user_name, date, comment}) => {
+                                return (
+                                    <Comments author={user_name} date={date.substr(0, 10)} text={comment}/>
+                                )
+                            })
+                        }
+                        <AddComment/>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
     }
-  }
-  
-  
-  class ShopInfo extends Component {
-    render(){
-      return (
-        <div className="shop-info-contact-detail">
-          <div>
-            <Icon name={this.props.icon} color={this.props.color}/>
-          </div>
-          <div>
-            {this.props.info}
-          </div>
-        </div>
-      )
+}
+
+
+class ShopInfo extends Component {
+    render() {
+        return (
+            <div className="shop-info-contact-detail">
+                <div>
+                    <Icon name={this.props.icon} color={this.props.color}/>
+                </div>
+                <div>
+                    {this.props.info}
+                </div>
+            </div>
+        )
     }
-  }
-  
-  class ShopProduct extends Component {
-    render(){
-      return (
-        <div className="shop-item">
-          <div className="shop-image">
-            <img src={this.props.image}/>
-          </div>
-  
-          <div className="shop-title">
-            <div>
+}
+
+class ShopProduct extends Component {
+    render() {
+        return (
+            <div className="shop-item">
+                <div className="shop-image">
+                    <img src={this.props.image}/>
+                </div>
+
+                <div className="shop-title">
+                    <div>
               <span className="shop-icon">
                 <Icon name="shopping bag" color="blue"/>
               </span>
-              {this.props.title}
-            </div>
-            
-          </div>
-  
-          <div className="product-price">
+                        {this.props.title}
+                    </div>
+
+                </div>
+
+                <div className="product-price">
             <span className="shop-icon">
               <Icon name="money bill alternate outline" color="green"/>
             </span>
-              {this.props.price}
-          </div>
-        </div>
-      )
+                    {this.props.price}
+                </div>
+            </div>
+        )
     }
-  }
+}
